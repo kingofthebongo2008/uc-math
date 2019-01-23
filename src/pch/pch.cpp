4acf34c7fb35e7cc97f681b96765cfeb3c9634f4
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-01-23 06:49:24 +0200

    fix build

--- src/pch/pch.cpp
+++ src/pch/pch.cpp
@@ -1,16 +1,3 @@
 #include "pch.h"
 
-#include <uc/os/windows/com_error.h>
-#include <uc/os/windows/com_initializer.h>
-
-#include <uc/sys/memcpy.h>
-#include <uc/sys/profile_timer.h>
-#include <uc/sys/spin_lock.h>
-
-#include <uc/util/bits.h>
-#include <uc/util/noncopyable.h>
-#include <uc/util/pimpl.h>
-#include <uc/util/pimpl_impl.h>
-#include <uc/util/utf8_conv.h>
-
-
+#include <uc/math/math.h>
